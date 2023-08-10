f75e03ae66489d5bc3f655187ec0aa758fb2707c
willyvaessen <github@willyvaessen.nl>
2023-08-10 21:41:33 +0200

    Made some nice progress on the medium screen size styling, while keeping the mobile screen styling intact. Time to call it a day.

--- 20230810_Day10/assets/scripts/dateTimeFunctions.js
+++ 20230810_Day10/assets/scripts/dateTimeFunctions.js
@@ -4,3 +4,8 @@ function getCurrentYear() {
     const currentYearElement = document.getElementById("currentYear");
     currentYearElement.textContent = new Date().getFullYear();
 }
+
+function getCurrentYearForSiteFooter() {
+    const currentYearElement = document.getElementById("currentYearSiteFooter");
+    currentYearElement.textContent = new Date().getFullYear();
+}
