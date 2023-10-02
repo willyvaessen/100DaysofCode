f0f24f02191fa087105d69c9e71f1ec1e83ec9e0
willyvaessen <github@willyvaessen.nl>
2023-10-02 11:05:04 +0200

    Day 62 ADded a form to add notes.

--- 20230930_Day60/myNotes/src/app/app.component.ts
+++ 20230930_Day60/myNotes/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import {NotesListComponent} from "../notes-list/notes-list.component";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AddNoteComponent} from "../add-note/add-note.component";
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, NotesListComponent, ReactiveFormsModule],
+  imports: [HeaderComponent, NotesListComponent, ReactiveFormsModule, AddNoteComponent],
 })
 export class AppComponent {
   name = new FormControl;
