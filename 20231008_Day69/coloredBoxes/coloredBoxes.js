560200cab22d74d4a071c1f609aefe8055e02579
willyvaessen <github@willyvaessen.nl>
2023-10-08 20:30:11 +0200

    Day 69 Progress is real. All toggles created.

--- 20231008_Day69/coloredBoxes/coloredBoxes.js
+++ 20231008_Day69/coloredBoxes/coloredBoxes.js
@@ -13,10 +13,10 @@ function toggleBoxColor(box) {
         box.style.backgroundColor = blueColor;
     }
 }
-function gameMove(firstBox, ...extraBoxes) {
+function multiToggle(...Boxes) {
     let boxID;
-    for (let i = 0; i < extraBoxes.length; i++) {
-        boxID = "#" + extraBoxes[i];
+    for (let i = 0; i < Boxes.length; i++) {
+        boxID = "#" + Boxes[i];
         const boxToChange = document.querySelector(boxID);
         toggleBoxColor(boxToChange);
     }
@@ -27,17 +27,41 @@ boxes.forEach(function (box) {
         const styles = e.currentTarget.classList;
 
         if (styles.contains("A1")) {
-            gameMove("A1", "B3", "C2", "D2", "D4");
+            multiToggle("A1", "B3", "C2", "D2", "D4");
         } else if (styles.contains("C1")) {
-            gameMove("C1", "A2", "A4", "B4", "D3");
+            multiToggle("C1", "A2", "A4", "B3", "B4", "D3");
         } else if (styles.contains("B3")) {
-            gameMove("C1", "D1", "B4", "B1", "C4");
+            multiToggle("C1", "D1", "B4", "B1", "C4");
         } else if (styles.contains("B4")) {
-            gameMove("B4", "A3", "B2", "B3", "C3");
+            multiToggle("B4", "A3", "B2", "B3", "C1", "C3");
+        } else if (styles.contains("A2")) {
+            multiToggle("A2", "B4", "D3", "C1");
+        } else if (styles.contains("A3")) {
+            multiToggle("A3", "B1", "C2", "D2", "C4");
+        } else if (styles.contains("A4")) {
+            multiToggle("C1", "D1", "B4", "B1", "C4");
+        } else if (styles.contains("B1")) {
+            multiToggle("B1","D2", "D4", "B4", "A3");
+        } else if (styles.contains("B2")) {
+            multiToggle("B2", "A4", "C3", "A2", "D1");
+        } else if (styles.contains("C2")) {
+            multiToggle("C2", "A1", "D4", "B3", "D1");
+        } else if (styles.contains("C3")) {
+            multiToggle("B1", "A4", "D3");
+        } else if (styles.contains("C4")) {
+            multiToggle("A3", "B1", "C2", "D2");
+        } else if (styles.contains("D1")) {
+            multiToggle("C1", "B4", "B1", "C4");
+        } else if (styles.contains("D2")) {
+            multiToggle("D2", "A4", "B1", "C3");
+        } else if (styles.contains("D3")) {
+            multiToggle("D3","A1", "B3", "C2", "A4");
+        } else if (styles.contains("D4")) {
+            multiToggle("D4", "A1", "A4", "B3", "C2", "D2");
         }
         // console.log("Event: "+e);
-        console.log(box);
-        toggleBoxColor(box);
+        // console.log(box);
+        // toggleBoxColor(box);
         attempts++;
         attemptsCounter.textContent = attempts;
     });
