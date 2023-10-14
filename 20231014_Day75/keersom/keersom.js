663b5d6e1907b55c4c36c60dbbc660ef4f444b95
willyvaessen <github@willyvaessen.nl>
2023-10-14 12:56:26 +0200

    Day 75 I seem to have it working for one assignent. Now to implement continuing to a next assignment.

--- 20231014_Day75/keersom/keersom.js
+++ 20231014_Day75/keersom/keersom.js
@@ -2,12 +2,15 @@ let leftTerm = document.getElementById("leftTerm");
 let rightTerm = document.getElementById("rightTerm");
 const resultContainer = document.getElementById("resultOutput");
 const userInputAnswer = document.getElementById("answer");
-const checkForm = document.getElementById("checkUserAnswer");
+let checkForm = document.getElementById("checkUserAnswer");
+let score = 0;
 
+//  This function returns a random number from 1 to 10
 function randomTerm() {
     return Math.floor((Math.random() * 10)+1);
 }
 
+//  This function creates the assignment to solve.
 function createAssignment() {
     let left = randomTerm();
     let right = randomTerm();
@@ -15,27 +18,41 @@ function createAssignment() {
     rightTerm.textContent = right.toString();
     return left * right;
 }
+// runPuzzle();
 
-
-
-
-checkForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    //  Handle submit
-    // checkAnswer()
-
-    resultContainer.textContent = checkAnswer();
-});
+//  The function below checks the user input against the outcome of the assignment.
 function checkAnswer() {
     let result;
     if (userInputAnswer.value === assignment.toString()) {
         console.log("Dat is goed.")
         result = "Goed";
+        score++;
     } else {
         console.log("Dat is helaas fout.")
         result = "Fout";
     }
     return result;
 }
+//  The following code handles the form submit button.
+//  Function
+checkForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //  Handle submit
+    // checkAnswer()
+    resultContainer.textContent = checkAnswer();
+    if (resultContainer.textContent === "Goed") {
+        resultContainer.style.color = "rgb(0,120,0)";
+        resultContainer.style.fontSize = "78px";
+    } else {
+        resultContainer.style.color = "rgb(120,0,0)";
+        resultContainer.style.fontSize = "78px";
+    }
+});
 
-let assignment = createAssignment();
+//  This function runs the actual puzzle.
+// function runPuzzle() {
+    let assignment = createAssignment();
+// }
+
+
+// createAssignment();
