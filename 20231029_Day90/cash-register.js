c9dc74a2c58e9754f19576be646bafeb60ab166d
WillyVaessen <github@WillyVaessen.nl>
2023-10-29 16:44:04 +0100

    Day 90
    Finally made it to the end of JavaScript Algorithms and Data Structures

--- 20231029_Day90/cash-register.js
+++ 20231029_Day90/cash-register.js
@@ -21,66 +21,83 @@
 
 //  Assignment code
 
+const currencyUnits = {
+    "ONE HUNDRED": 100,
+    "TWENTY": 20,
+    "TEN": 10,
+    "FIVE": 5,
+    "ONE": 1,
+    "QUARTER": 0.25,
+    "DIME": 0.10,
+    "NICKEL": 0.05,
+    "PENNY": 0.01
+}
+
+const currencyUnits100 = {
+    "ONE HUNDRED": 10000,
+    "TWENTY": 2000,
+    "TEN": 1000,
+    "FIVE": 500,
+    "ONE": 100,
+    "QUARTER": 25,
+    "DIME": 10,
+    "NICKEL": 5,
+    "PENNY": 1
+}
+
 function checkCashRegister(price, cash, cid) {
-  let change = cash - price;
-  // console.log(change);
-  let cidTotal = 0;
-  for (let elem of cid){
-    // console.log(elem);
-    cidTotal += elem[1];
-    // console.log(cidTotal);
-  }
-  if(change > cidTotal){
-    return {status: "INSUFFICIENT_FUNDS", change: []}
-  } else if (change === cidTotal) {
-      return {status: "CLOSED", change: cid}
-  } else {
-      let result = [];
-      console.log(cid)
-      cid = cid.reverse();
-      console.log(cid);
-      console.log("Change is: " + change);
-      let currencyUnits = {
-          "ONE HUNDRED": 100,
-          "TWENTY": 20,
-          "TEN": 10,
-          "FIVE": 5,
-          "ONE": 1,
-          "QUARTER": 0.25,
-          "DIME": 0.10,
-          "NICKEL": 0.05,
-          "PENNY": 0.01
-      }
-        for (let elem of cid){
-            let money = [elem[0],0]
-            while (change >= currencyUnits[elem[0]] && elem[1] > 0) {
-                change -= currencyUnits[elem[0]];
-                elem[1] -= currencyUnits[elem[0]];
-                money[1] += currencyUnits[elem[0]];
+    let change = cash * 100 - price * 100;
+    console.log(change);
+    let cidTotalValue = 0;
+    for (let elem of cid) {
+        // console.log(elem);
+        cidTotalValue += elem[1] * 100;
+        console.log(cidTotalValue);
+    }
+    console.log(change);
+    if (change > cidTotalValue) {
+        return {status: "INSUFFICIENT_FUNDS", change: []}
+    } else if (change === cidTotalValue) {
+        return {status: "CLOSED", change: cid}
+    } else {
+        let result = [];
+        cid = cid.reverse();    //  Reversing the values in cid, to display from largest to smallest.
+        for (let elem of cid) {
+            let money = [elem[0], 0]
+            elem[1] = elem[1] * 100;
+            while (change >= currencyUnits100[elem[0]] && elem[1] > 0) {
+                change -= currencyUnits100[elem[0]];
+                elem[1] -= currencyUnits100[elem[0]];
+                money[1] += currencyUnits100[elem[0]] / 100;
             }
             if (money[1] > 0) {
                 result.push(money);
             }
-        } return {status: "OPEN", change: result}
-  }
+
+        }
+        // if (change >0){
+        //     return {status: "INSUFFICIENT_FUNDS", change: []}
+        // }
+        return {status: "OPEN", change: result}
+    }
 }
 
 
-//  Tests
+// Tests
 console.log("1: ----------------------------------------")
-console.log(checkCashRegister(19.5, 20,  [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 console.log("")
 console.log("2: ----------------------------------------")
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 console.log("")
 console.log("3: ----------------------------------------")
-console.log(checkCashRegister(19.5, 20,  [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 console.log("")
 console.log("4: ----------------------------------------")
-console.log(checkCashRegister(19.5, 20,  [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 console.log("")
 console.log("5: ----------------------------------------")
-console.log(checkCashRegister(19.5, 20,  [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 console.log("")
 
 
