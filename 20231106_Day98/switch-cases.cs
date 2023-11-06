af9dd260466fb0cca87b69d6d775062bdde87358
WillyVaessen <github@WillyVaessen.nl>
2023-11-06 18:18:58 +0100

    Day 98 Branch the Flow of Code Using the switch-case Construct in C#

---
+++ 20231106_Day98/switch-cases.cs
@@ -0,0 +1,29 @@
+//  Branch the Flow of Code Using the switch-case Construct in C#
+
+/*  Exercise - Implement a switch statement
+
+int employeeLevel = 201;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+*/
+
