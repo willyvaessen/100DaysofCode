b226c3202dc08cb5090ef2b7d4ae71429aab715c
WillyVaessen <github@WillyVaessen.nl>
2023-11-07 16:30:16 +0100

    Day 99 Worming with input validation in C# Project 2

--- 20231107_Day99/input-validation.cs
+++ 20231107_Day99/input-validation.cs
@@ -38,12 +38,49 @@ do
     }
 } while (validEntry == false);
 */
-
 /*  Capture user input in a string variable named readResult
 int numericValue = 0;
 bool validNumber = false;
 
 validNumber = int.TryParse(readResult, out numericValue);
 */
-
 /*  Code project 1 - write code that validates integer input
+string? readResult;
+int numericValue = 0;
+bool validInt = false;
+Console.WriteLine("Enter an integer between 5 and 10:");
+do {
+    readResult = Console.ReadLine();
+    // Console.WriteLine(int.TryParse(readResult, out numericValue));
+    if (int.TryParse(readResult, out numericValue)) {
+        // Console.WriteLine("Integer entered is: " + numericValue);
+        if ((numericValue < 5) || (numericValue > 10)) {
+            // Console.WriteLine("Enter an integer between 5 and 10:");
+        } else if ((numericValue >= 5) || (numericValue <= 10)) {
+            Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+            validInt = true;
+        } else {
+            Console.WriteLine("Not sure what you're doing, but it's not right.");
+        }
+    } else {
+        Console.WriteLine("Enter a valid integer.");
+    }
+
+} while (validInt == false);
+*/
+/*  Code project 2 - write code that validates string input
+string? readResult;
+bool validInput = false;
+Console.WriteLine("Enter your role (Adminstrator, Manager or User): ");
+do {
+    readResult = Console.ReadLine();
+    if ((readResult.Trim().ToLower() == "administrator") || (readResult.Trim().ToLower() == "manager") || (readResult.Trim().ToLower() == "user")) {
+        Console.WriteLine($"Your input ({readResult}) has been accepted.");
+        validInput = true;
+    } else {
+        Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+    }
+
+} while (validInput == false);
+*/
+
